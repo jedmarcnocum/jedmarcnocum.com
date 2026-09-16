2f6adcf590aea268b47af2b6aa765f9c6245b656
Jed-Marc Nocum <jedmarcnocum@outlook.com>
2026-09-16 11:09:51 +1000

    - updated lightbox to gallery

--- assets/blog-image-modal.js
+++ assets/blog-image-modal.js
@@ -12,25 +12,69 @@
   dialog.setAttribute("aria-label", "Image preview");
   dialog.innerHTML = `
     <div class="blog-image-modal__content">
-      <img class="blog-image-modal__image" alt="" />
-      <button class="blog-image-modal__close" type="button" aria-label="Close image preview">
-        <span aria-hidden="true">×</span>
-      </button>
+      <div class="blog-image-modal__stage">
+        <img class="blog-image-modal__image" alt="" />
+        <button class="blog-image-modal__control blog-image-modal__control--previous" type="button" aria-label="Previous image">
+          <span aria-hidden="true">&larr;</span>
+        </button>
+        <button class="blog-image-modal__control blog-image-modal__control--next" type="button" aria-label="Next image">
+          <span aria-hidden="true">&rarr;</span>
+        </button>
+        <button class="blog-image-modal__close" type="button" aria-label="Close image preview">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="blog-image-modal__gallery" role="group" aria-label="Images in this post"></div>
     </div>
   `;
 
   document.body.append(dialog);
 
   const modalImage = dialog.querySelector(".blog-image-modal__image");
+  const previousButton = dialog.querySelector(".blog-image-modal__control--previous");
+  const nextButton = dialog.querySelector(".blog-image-modal__control--next");
   const closeButton = dialog.querySelector(".blog-image-modal__close");
+  const gallery = dialog.querySelector(".blog-image-modal__gallery");
   let opener;
+  let activeIndex = 0;
 
   const closeModal = () => dialog.close();
 
-  const openModal = (image) => {
-    opener = image;
+  const thumbnails = images.map((image, index) => {
+    const thumbnail = document.createElement("button");
+    thumbnail.className = "blog-image-modal__thumbnail";
+    thumbnail.type = "button";
+    thumbnail.setAttribute(
+      "aria-label",
+      `View image ${index + 1} of ${images.length}: ${image.alt || "blog image"}`,
+    );
+
+    const thumbnailImage = document.createElement("img");
+    thumbnailImage.src = image.currentSrc || image.src;
+    thumbnailImage.alt = "";
+    thumbnailImage.loading = "lazy";
+    thumbnail.append(thumbnailImage);
+    gallery.append(thumbnail);
+    return thumbnail;
+  });
+
+  const setActiveImage = (index) => {
+    activeIndex = (index + images.length) % images.length;
+    const image = images[activeIndex];
+
     modalImage.src = image.currentSrc || image.src;
     modalImage.alt = image.alt;
+    thumbnails.forEach((thumbnail, thumbnailIndex) => {
+      const isActive = thumbnailIndex === activeIndex;
+      thumbnail.classList.toggle("is-active", isActive);
+      thumbnail.setAttribute("aria-pressed", String(isActive));
+    });
+    thumbnails[activeIndex].scrollIntoView({ block: "nearest", inline: "center" });
+  };
+
+  const openModal = (image) => {
+    opener = image;
+    setActiveImage(images.indexOf(image));
     dialog.showModal();
     closeButton.focus();
   };
@@ -54,9 +98,21 @@
     });
   });
 
+  thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", () => setActiveImage(index));
+  });
+  previousButton.addEventListener("click", () => setActiveImage(activeIndex - 1));
+  nextButton.addEventListener("click", () => setActiveImage(activeIndex + 1));
   closeButton.addEventListener("click", closeModal);
   dialog.addEventListener("click", (event) => {
     if (event.target === dialog) closeModal();
+  });
+  dialog.addEventListener("keydown", (event) => {
+    if (event.key !== "ArrowLeft" && event.key !== "ArrowRight") return;
+
+    event.preventDefault();
+    setActiveImage(activeIndex + (event.key === "ArrowRight" ? 1 : -1));
+    thumbnails[activeIndex].focus();
   });
   dialog.addEventListener("close", () => opener?.focus());
 })();
